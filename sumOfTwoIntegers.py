4f92f08571366dc5210f65b362a99325a6432189
Rhiana Latifh Nakalemba <rhiana.nakalemba@ashesi.edu.gh>
2021-01-04 19:33:48 +0300

    Added sumOfTwoIntegers.py

---
+++ sumOfTwoIntegers.py
@@ -0,0 +1,13 @@
+'''
+Given an array of integers and a value, determine if there are any 
+two integers in the array whose sum is equal to the given value.
+'''
+
+def sumOfTwoIntegers(array, value):
+    
+
+
+
+a = []
+n = 3
+print(sumOfTwoIntegers(a, n))
